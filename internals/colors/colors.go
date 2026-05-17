95f4903442ba7001f2e2c8d12c2d9a5fabc82315
Muhammad Ismail <m.ismailusman47@gmail.com>
2026-05-17 20:01:01 +0500

    feat: ANSI color codes for terminal output

---
+++ internals/colors/colors.go
@@ -0,0 +1,11 @@
+package colors
+
+// Standard ANSI color codes for terminal output
+const (
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Cyan   = "\033[36m"
+	Yellow = "\033[33m"
+	Green  = "\033[32m"
+	Gray   = "\033[90m"
+)
